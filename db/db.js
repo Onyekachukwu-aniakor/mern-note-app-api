4f023953faf9fcb42d6ed125dea57e5c4adcb9b7
Onyekachukwu Aniakor <carlosaniakorchukwu@gmail.com>
2025-12-16 20:38:42 +0000

    first commit

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectToMongoDB = async ()=>{
     try {
-        await mongoose.connect("mongodb://localhost:27017/Mern-Note-App");
+        await mongoose.connect("mongodb+srv://Onyekachukwu:Carlos.042@cluster0.4kaqz2b.mongodb.net/Mern-Note-App?appName=Cluster0");
         console.log('Connected To MongoDB')
     } catch (error) {
         console.log('Error connecting to MongoDB')
